8f4f39d70e0de85d26a2116be5522e5aed8cfc27
Nat <nat@cmmc.io>
2017-11-06 10:09:16 +0700

    update static files

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 rsync --exclude=.git --delete -av src/netpie-client-basic/ netpie/client/ 
 rsync --exclude=.git --delete -av src/hivemq-mqtt-web-client/ mqtt/client/ 
+ln -s netpie/client netpie-client
+ln -s src/espnow-sensor-node-graph/public sensor-nodes
 git commit -am 'update static files' 
 
