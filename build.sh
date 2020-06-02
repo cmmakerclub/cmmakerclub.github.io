854e07082aa7312b81f29c346a87c718f89f6ef0
Nat <nat.wrw@gmail.com>
2020-06-02 21:15:47 +0700

    updat build script.

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-rsync --exclude=.git --delete -av src/netpie-client-basic/ netpie/client/ 
-rsync --exclude=.git --delete -av src/hivemq-mqtt-web-client/ mqtt/client/ 
-ln -s netpie/client netpie-client
-ln -s src/espnow-sensor-node-graph/public sensor-nodes
+#rsync --exclude=.git --delete -av src/netpie-client-basic/ netpie/client/ 
+#rsync --exclude=.git --delete -av src/hivemq-mqtt-web-client/ mqtt/client/ 
+
+#ln -s netpie/client netpie-client
+#ln -s src/espnow-sensor-node-graph/public sensor-nodes
+ln -sf src/vue-iot-devices-color-picker mqtt/devices-2
 git commit -am 'update static files' 
 
